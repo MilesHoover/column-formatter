a770537e33bef67c3a11d28b97443657413d5be6
miles <mhoover709@gmail.com>
2026-09-06 18:53:42 -0400

    add logging, type-hints

--- src/sku_formatter/sku_formatter.py
+++ src/sku_formatter/sku_formatter.py
@@ -1,44 +1,60 @@
 import re
+import os
+import logging
+from time import perf_counter
 
-def driver(file_name):
+
+def driver(file_name: str | os.PathLike) -> list:
     try:
+        start = perf_counter()
+
         raw = extract_skus(file_name)
         formatted = format_skus(raw)
         write_skus(formatted, file_name)
+
+        end = perf_counter()
     except (FileNotFoundError, PermissionError, IsADirectoryError, UnicodeDecodeError):
-        print("Double check your file and try again")
+        logging.error("Double check your input file and try again")
         exit()
+
+    logging.info("Format Completed in %.4f", end - start)
 
     return formatted
 
-def extract_skus(file_name):
+
+def extract_skus(file_name: str | os.PathLike) -> list[str]:
     raw_sku_list = []
 
     with open(file_name, "r") as file:
         for line in file:
-            raw_skus = re.findall(r"\d+", line) # "\d" means decimal digits and "+" means 1 or more times
+            # "\d" means decimal digits and "+" means 1 or more times
+            raw_skus = re.findall(r"\d+", line)
             for sku in raw_skus:
                 raw_sku_list.append(sku)
 
     return raw_sku_list
 
-def format_skus(raw_sku_list):
+
+def format_skus(raw_sku_list: list) -> list[str]:
     formatted_sku_list = []
 
     for raw_sku in raw_sku_list:
-        stripped_sku = re.sub('^0+', '', raw_sku) # ^ means starts with 0 and replaces it with nothing (stripping the zeros)
+        # ^ means starts with 0 and replaces it with nothing (stripping the zeros)
+        stripped_sku = re.sub('^0+', '', raw_sku)
         if stripped_sku == '':
             continue
         else:
             formatted_sku_list.append(stripped_sku)
 
-    deduped_sku_dictionary = dict.fromkeys(formatted_sku_list, 0) # dictionaries dedupe and keep order
+    # dictionaries dedupe and keep order
+    deduped_sku_dictionary = dict.fromkeys(formatted_sku_list, 0)
 
     deduped_sku_list = list(deduped_sku_dictionary.keys())
 
     return deduped_sku_list
 
-def write_skus(deduped_sku_list, file_name):
+
+def write_skus(deduped_sku_list: list, file_name: str | os.PathLike) -> None:
     with open(file_name, "w") as file:
         for index, sku in enumerate(deduped_sku_list):
             formatted_sku = (f"'{sku}'")
@@ -46,4 +62,3 @@ def write_skus(deduped_sku_list, file_name):
                 file.write(f"{formatted_sku},\n")
             else:
                 file.write(formatted_sku)
-    print("Format complete")
